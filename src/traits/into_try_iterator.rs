cc9f2e4fd166b1648786378b6b11043497f43c65
Tim Vermeulen <tvermeulen@me.com>
2019-08-24 18:32:14 +0200

    Add IntoTryIterator impls

--- src/traits/into_try_iterator.rs
+++ src/traits/into_try_iterator.rs
@@ -20,3 +20,54 @@ where
         self
     }
 }
+
+mod result {
+    use super::*;
+
+    impl<T, E> IntoTryIterator for Result<T, E> {
+        type Item = T;
+        type Error = E;
+        type IntoTryIter = Iter<T, E>;
+
+        fn into_try_iter(self) -> Self::IntoTryIter {
+            Iter(Some(self))
+        }
+    }
+
+    pub struct Iter<T, E>(Option<Result<T, E>>);
+
+    impl<T, E> TryIterator for Iter<T, E> {
+        type Item = T;
+        type Error = E;
+
+        fn next(&mut self) -> Result<Option<Self::Item>, Self::Error> {
+            self.0.take().transpose()
+        }
+    }
+}
+
+mod option {
+    use super::*;
+    use core::option::NoneError;
+
+    impl<T> IntoTryIterator for Option<T> {
+        type Item = T;
+        type Error = NoneError;
+        type IntoTryIter = Iter<T>;
+
+        fn into_try_iter(self) -> Self::IntoTryIter {
+            Iter(self.into_result().into_try_iter())
+        }
+    }
+
+    pub struct Iter<T>(result::Iter<T, NoneError>);
+
+    impl<T> TryIterator for Iter<T> {
+        type Item = T;
+        type Error = NoneError;
+
+        fn next(&mut self) -> Result<Option<Self::Item>, Self::Error> {
+            self.0.next()
+        }
+    }
+}
